6dc25c8a9413320f6a7be4ac66a577aa33c761f2
Pere <pere.solaclaver@gmail.com>
2020-03-21 17:10:09 +0000

    Install helmet

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 // DEPENDENCIES
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 // INSTANTIATE EXPRESS TO CREATE SERVER
 const server = express();
 
 // MIDDLEWARE
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
