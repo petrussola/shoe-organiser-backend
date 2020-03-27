c0e4a6d0455dfb7bee8c9836eb4fcf6c584a0baf
Pere <pere.solaclaver@gmail.com>
2020-03-27 08:42:46 +0000

    Login functionality

---
+++ api/helpers/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+async function generateToken(user) {
+    const payload = {
+        email: user.email,
+        name: user.name,
+    };
+    const options = {
+        expiresIn: '5d',
+    };
+    try {
+        const result = jwt.sign(payload, process.env.JWT_SECRET, options);
+        return result;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+module.exports = {
+    generateToken,
+};
--- api/routes/authroute.js
+++ api/routes/authroute.js
@@ -2,12 +2,13 @@
 const express = require('express');
 
 // CONTROLLERS
-const { createUser } = require('../controllers/user.js');
+const { createUser, loginUser } = require('../controllers/user.js');
 
 // INSTANTIATE ROUTER
 const router = express.Router();
 
 // ENDPOINTS
 router.post('/register', createUser);
+router.post('/login', loginUser);
 
 module.exports = router;
