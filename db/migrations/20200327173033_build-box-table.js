125c2c021cdb87130b449fc869ed6a69bbdb5ecc
Pere <pere.solaclaver@gmail.com>
2020-03-28 23:45:35 +0000

    Set box_number as unsigned

--- db/migrations/20200327173033_build-box-table.js
+++ db/migrations/20200327173033_build-box-table.js
@@ -2,7 +2,10 @@
 exports.up = function(knex) {
     return knex.schema.createTable('boxes', table => {
         table.increments('id');
-        table.integer('box_number').notNullable();
+        table
+            .integer('box_number')
+            .unsigned()
+            .notNullable();
     });
 };
 
