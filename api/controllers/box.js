b044183bd513c72737f92b657d29f02bd559352d
Pere <pere.solaclaver@gmail.com>
2020-03-28 21:41:33 +0000

    Create error path when retrieving boxes

--- api/controllers/box.js
+++ api/controllers/box.js
@@ -6,7 +6,10 @@ async function getBoxes(req, res) {
         const boxes = await Box.getAllBoxes();
         res.status(200).json(boxes);
     } catch (error) {
-        console.log(error);
+        res.status(500).json({
+            status: 'fail',
+            message: `There was a problem fetching the boxes: ${error.message}`,
+        });
     }
 }
 // async function createBox(req, res) {}
